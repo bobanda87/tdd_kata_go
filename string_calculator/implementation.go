e32f4156c088fc55b6ce097d9e29a1769c48810d
boban.da87 <boban.da87@gmail.com>
2025-02-12 13:46:41 +0100

    chore: Kata(5)

--- string_calculator/implementation.go
+++ string_calculator/implementation.go
@@ -1,5 +1,34 @@
 package string_calculator
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Add(expr string) int64 {
-	return -1
+	if expr == "" {
+		return 0
+	}
+
+	delimiter := ","
+	if strings.HasPrefix(expr, "//") {
+		delimiter_start := 2
+		if expr[delimiter_start] == '[' {
+			delimiter_end := strings.Index(expr, "]")
+			delimiter = string(expr[delimiter_start+1 : delimiter_end])
+			expr = strings.Replace(expr, expr[0:delimiter_end+1], "", -1)
+		} else {
+			delimiter = string(expr[2])
+			expr = strings.Replace(expr, "//"+delimiter, "", -1)
+		}
+	}
+
+	numbers := strings.Split(strings.Replace(expr, "\n", delimiter, -1), delimiter)
+	sum := int64(0)
+	for i := 0; i < len(numbers); i++ {
+		i, _ := strconv.ParseInt(numbers[i], 10, 64)
+		sum += i
+	}
+
+	return sum
 }
--- string_calculator/implementation_test.go
+++ string_calculator/implementation_test.go
@@ -17,6 +17,41 @@ func TestAdd(t *testing.T) {
 			"",
 			0,
 		},
+		{
+			"two numbers",
+			"1,2",
+			3,
+		},
+		{
+			"one number",
+			"1",
+			1,
+		},
+		{
+			"3 numbers",
+			"1,2,3",
+			6,
+		},
+		{
+			"4 numbers",
+			"1,2,3,4",
+			10,
+		},
+		{
+			"strange scenario",
+			"1\n2,3",
+			6,
+		},
+		{
+			"new delimiter",
+			"//;\n1;2",
+			3,
+		},
+		{
+			"new delimiter with brackets",
+			"//[***]\n1***2***3",
+			6,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
